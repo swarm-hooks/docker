6701a9f6af31cdc8dcfd9f5696116e3f037cde50
Morgan Bauer <mbauer@us.ibm.com>
2015-08-04 12:02:46 -0700

    Adding commit fad4071

--- daemon/exec.go
+++ daemon/exec.go
@@ -128,7 +128,7 @@ func (d *Daemon) getActiveContainer(name string) (*Container, error) {
 	return container, nil
 }
 
-// ContainerExecCreate sets up an exec in a running container. 
+// ContainerExecCreate sets up an exec in a running container.
 func (d *Daemon) ContainerExecCreate(config *runconfig.ExecConfig) (string, error) {
 	// Not all drivers support Exec (LXC for example)
 	if err := checkExecSupport(d.execDriver.Name()); err != nil {
