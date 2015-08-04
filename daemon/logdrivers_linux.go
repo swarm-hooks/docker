b4233a882148cd64a19c60f1659c2ce0aeaf1cba
Morgan Bauer <mbauer@us.ibm.com>
2015-08-04 12:02:47 -0700

    Adding commit 447dae9

--- daemon/logdrivers_linux.go
+++ daemon/logdrivers_linux.go
@@ -1,8 +1,8 @@
 package daemon
 
 import (
-// Importing packages here only to make sure their init gets called and
-// therefore they register themselves to the logdriver factory.
+	// Importing packages here only to make sure their init gets called and
+	// therefore they register themselves to the logdriver factory.
 	_ "github.com/docker/docker/daemon/logger/fluentd"
 	_ "github.com/docker/docker/daemon/logger/gelf"
 	_ "github.com/docker/docker/daemon/logger/journald"
